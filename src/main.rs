c0c55f14503086d310186e3c0d0d0b3edb3854df
JavierPoduje <javier.poduje@gmail.com>
2022-10-27 21:51:11 -0300

    feat: add day_6

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,36 @@ mod day_2;
 mod day_3;
 mod day_4;
 mod day_5;
+mod day_6;
 
 fn main() {
-    let (part1, part2) = day_1::solution::solution();
-    println!("- Day 1");
-    println!("\t* part 1: {}", part1);
-    println!("\t* part 2: {}", part2);
+    //let (_part1, _part2) = day_1::solution::solution();
+    //println!("- Day 1");
+    //println!("\t* part 1: {}", _part1);
+    //println!("\t* part 2: {}", _part2);
 
-    let (part1, part2) = day_2::solution::solution();
-    println!("- Day 2");
-    println!("\t* part 1: {}", part1);
-    println!("\t* part 2: {}", part2);
+    //let (_part1, _part2) = day_2::solution::solution();
+    //println!("- Day 2");
+    //println!("\t* part 1: {}", _part1);
+    //println!("\t* part 2: {}", _part2);
 
-    let (part1, part2) = day_3::solution::solution();
-    println!("- Day 3");
-    println!("\t* part 1: {}", part1);
-    println!("\t* part 2: {}", part2);
+    //let (_part1, _part2) = day_3::solution::solution();
+    //println!("- Day 3");
+    //println!("\t* part 1: {}", _part1);
+    //println!("\t* part 2: {}", _part2);
 
-    let (part1, part2) = day_4::solution::solution();
-    println!("- Day 4");
-    println!("\t* part 1: {}", part1);
-    println!("\t* part 2: {}", part2);
+    //let (_part1, _part2) = day_4::solution::solution();
+    //println!("- Day 4");
+    //println!("\t* part 1: {}", _part1);
+    //println!("\t* part 2: {}", _part2);
 
-    let (part1, part2) = day_5::solution::solution();
-    println!("- Day 5");
+    //let (_part1, _part2) = day_5::solution::solution();
+    //println!("- Day 5");
+    //println!("\t* part 1: {}", _part1);
+    //println!("\t* part 2: {}", _part2);
+
+    let (part1, part2) = day_6::solution::solution();
+    println!("- Day 6");
     println!("\t* part 1: {}", part1);
     println!("\t* part 2: {}", part2);
 }
