706afda00bd6f4598cc9ef79a6e168d0264ab333
JavierPoduje <javier.poduje@gmail.com>
2022-12-04 17:29:47 -0300

    feat: add 2022, day 1

--- src/day_1/solution.rs
+++ src/day_1/solution.rs
@@ -1,6 +1,25 @@
 use super::super::utils::read_one_per_line::read_one_per_line;
 
 pub fn solution() -> (u64, u64) {
-    let dummy = read_one_per_line::<u32>("./src/day_1/input.txt").unwrap();
-    (0, 0)
+    let rows = read_one_per_line::<String>("./src/day_1/input.txt").unwrap();
+
+    let mut calories = Vec::<u64>::new();
+    let mut calorie: u64 = 0;
+
+    for row in rows {
+        match row.as_str() {
+            "" => {
+                calories.push(calorie);
+                calorie = 0;
+            }
+            _ => {
+                let current: u64 = row.parse().unwrap();
+                calorie += current;
+            }
+        }
+    }
+
+    let max: u64 = calories.iter().max().unwrap().clone();
+
+    (max, 0)
 }
