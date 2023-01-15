d8e3ae0a09f02080c0093fb3c7aae88c9c803396
JavierPoduje <javier.poduje@gmail.com>
2023-01-15 15:41:07 -0300

    feat: add 2022's day 14 - part 2

--- src/day_14/solution.rs
+++ src/day_14/solution.rs
@@ -3,21 +3,31 @@ use std::{cmp::max, collections::HashMap};
 use super::super::utils::read_one_per_line::read_one_per_line;
 
 pub fn solution() -> (String, String) {
-    let (cave, bottom) = parse();
-    let count = part1(cave, bottom);
-    (count.to_string(), "B".to_string())
+    let cave = parse();
+    let count = part1(cave.clone());
+    let part2 = part2(cave);
+    (count.to_string(), part2.to_string())
 }
 
-fn part1(mut cave: Cave, bottom: i32) -> i32 {
+fn part1(mut cave: Cave) -> i32 {
     let mut count = 0;
-    while cave.drop_one(bottom) {
+    while cave.drop_one() {
+        count += 1;
+    }
+    count
+}
+
+fn part2(mut cave: Cave) -> i32 {
+    let mut count = 0;
+    cave.has_floor = true;
+    while cave.drop_one() {
         count += 1;
     }
     cave.display();
     count
 }
 
-fn parse() -> (Cave, i32) {
+fn parse() -> Cave {
     let lines: Vec<String> = read_one_per_line::<String>("./src/day_14/input.txt")
         .unwrap()
         .into_iter()
@@ -25,24 +35,25 @@ fn parse() -> (Cave, i32) {
         .collect();
 
     let mut cave = Cave::default();
-    let mut bottom = 0;
 
     for line in lines {
         let mut iter = line.split(" -> ");
         let mut start = Cave::convert(iter.next()).unwrap();
         while let Some(end) = Cave::convert(iter.next()) {
             cave.draw_line(start, end);
-            bottom = max(start.1, end.1);
+            cave.bottom = max(start.1, end.1);
             start = end;
         }
     }
 
-    (cave, bottom)
+    cave
 }
 
 #[derive(Default, Clone)]
 struct Cave {
     tile: HashMap<(i32, i32), char>,
+    bottom: i32,
+    has_floor: bool,
 }
 
 impl Cave {
@@ -55,13 +66,21 @@ impl Cave {
         }
     }
 
-    pub fn drop_one(&mut self, bottom: i32) -> bool {
+    pub fn drop_one(&mut self) -> bool {
         let mut sand = (500, 0);
+
+        if self.has_floor && self.tile.contains_key(&sand) {
+            return false;
+        }
+
         while let Some(next_pos) = self.fall(sand) {
-            if next_pos.1 > bottom {
+            if !self.has_floor && next_pos.1 > self.bottom {
                 return false;
             }
             sand = next_pos;
+            if self.has_floor && sand.1 == self.bottom + 1 {
+                break;
+            }
         }
         self.tile.insert(sand, 'o');
         true
@@ -90,13 +109,11 @@ impl Cave {
             point.1 += dy;
             self.tile.insert(point, '#');
         }
-
-        println!("Draw line from {start:?} to {end:?}");
     }
 
     fn display(&self) {
-        for y in 0..=9 {
-            for x in 494..=503 {
+        for y in 0..=self.bottom + 1 {
+            for x in 480..=520 {
                 print!(
                     "{}",
                     if x == 500 && y == 0 {
