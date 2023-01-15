d3ccbaad7e1304df3ca1d6989c4f526bf0b364fb
JavierPoduje <javier.poduje@gmail.com>
2023-01-15 19:32:02 -0300

    feat: add 2022's day 15 - part 2

--- src/day_15/solution.rs
+++ src/day_15/solution.rs
@@ -3,11 +3,16 @@ use std::collections::HashSet;
 use super::super::utils::read_one_per_line::read_one_per_line;
 
 const PART1_ROW: i64 = 2000000;
+const PART2_MAX: i64 = 4000000;
+
+//const PART1_ROW: i64 = 10;
+//const PART2_MAX: i64 = 20;
 
 pub fn solution() -> (String, String) {
     let sensors = parse();
-    let part1 = part1(sensors);
-    (part1.to_string(), "B".to_string())
+    let part1 = part1(sensors.clone());
+    let part2 = part2(sensors);
+    (part1.to_string(), part2.to_string())
 }
 
 fn part1(sensors: Vec<Sensor>) -> usize {
@@ -31,11 +36,60 @@ fn part1(sensors: Vec<Sensor>) -> usize {
         sensors
             .iter()
             .filter(|s| s.beacon.1 == PART1_ROW)
-            .map(|s| s.beacon.0)
-            .collect::<Vec<i64>>(),
+            .map(|s| s.beacon.0),
     );
 
     coverage.len() - beacons.len()
+}
+
+fn part2(sensors: Vec<Sensor>) -> i64 {
+    for row in 0..=PART2_MAX {
+        let mut rowdata = vec![0..=PART2_MAX];
+        for s in &sensors {
+            let radius = s.radius();
+            let top = 0.max(s.loc.1 - radius);
+            let bottom = PART2_MAX.min(s.loc.1 + radius);
+
+            if top > row || bottom < row {
+                continue;
+            }
+
+            let dist = (s.loc.1 - row).abs();
+            let min_x = 0.max(s.loc.0 - (radius - dist));
+            let max_x = PART2_MAX.min(s.loc.0 + (radius - dist));
+
+            let mut new_range = Vec::new();
+
+            for r in &rowdata {
+                let start = *r.start();
+                if start > max_x {
+                    new_range.push(r.clone());
+                    continue;
+                }
+
+                let end = *r.end();
+                if end < min_x {
+                    new_range.push(r.clone());
+                    continue;
+                }
+
+                if start < min_x {
+                    new_range.push(start..=min_x - 1);
+                }
+
+                if end > max_x {
+                    new_range.push(max_x + 1..=end);
+                }
+            }
+            rowdata = new_range;
+        }
+        if !rowdata.is_empty() {
+            let x = *rowdata[0].start();
+            return x * PART2_MAX + row;
+        }
+    }
+
+    unreachable!()
 }
 
 fn parse() -> Vec<Sensor> {
@@ -46,12 +100,9 @@ fn parse() -> Vec<Sensor> {
         .collect();
 
     let mut sensors = Vec::new();
-
     for line in lines {
-        let sensor = Sensor::parse(&line);
-        sensors.push(sensor);
+        sensors.push(Sensor::parse(&line));
     }
-
     sensors
 }
 
