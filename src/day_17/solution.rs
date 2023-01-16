8ffb23d4573c76109bf7df94abf1d4773e6f4bfe
JavierPoduje <javier.poduje@gmail.com>
2023-01-16 20:47:52 -0300

    feat: add 2022's day 17 - part 2

--- src/day_17/solution.rs
+++ src/day_17/solution.rs
@@ -1,20 +1,71 @@
+use std::collections::HashMap;
+
 use super::super::utils::read_one_per_line::read_one_per_line;
 
 pub fn solution() -> (String, String) {
     let jets = parse();
     let chamber = Chamber::new(jets);
-    let part1 = part1(chamber);
-    (part1.to_string(), "B".to_string())
+    let part1 = part1(chamber.clone());
+    let part2 = part2(chamber);
+    (part1.to_string(), part2.to_string())
 }
 
-fn part1(chamber: Chamber) -> usize {
-    let mut part1 = chamber.clone();
-
+fn part1(mut part1: Chamber) -> usize {
     for _ in 0..2022 {
         part1.drop_one();
     }
-    //part1._draw();
     part1.height()
+}
+
+fn part2(mut part2: Chamber) -> usize {
+    let mut cycle_finder = HashMap::new();
+    cycle_finder.insert((part2.piecenum, part2.jetnum, 0usize), (0usize, 0usize));
+
+    let mut drops = 0;
+    loop {
+        part2.drop_one();
+        drops += 1;
+        let height = part2.height();
+
+        if height < 4 {
+            continue;
+        }
+
+        let shape = ((part2.rocks[height - 1] as usize) << 24)
+            | ((part2.rocks[height - 2] as usize) << 16)
+            | ((part2.rocks[height - 3] as usize) << 8)
+            | (part2.rocks[height - 4]) as usize;
+
+        if let Some(entry) = cycle_finder.get(&(part2.piecenum, part2.jetnum, shape)) {
+            //println!("piece = {}", part2.piecenum);
+            //println!("jetnum = {}", part2.jetnum);
+            //println!("shape = {}", shape);
+            //println!("drops until start of loop = {}", entry.1);
+            //println!("height of the tower when the loop started = {}", entry.0);
+
+            let delta_height = height - entry.0;
+            let delta_drops = drops - entry.1;
+            //println!("There's an increase of {delta_height} rows for every {delta_drops} drops");
+            let remaining_drops = Chamber::OUCH - entry.1;
+            //println!("There're still {remaining_drops} left to go");
+
+            let needed_drops = remaining_drops / delta_drops;
+            let leftover_drops = remaining_drops % delta_drops;
+            let integral_height = entry.0 + delta_height * needed_drops;
+
+            for _ in 0..leftover_drops {
+                part2.drop_one();
+            }
+
+            let leftover_height = part2.height() - height;
+
+            //println!("After {leftover_drops} mmore drops , we added {leftover_height} rows");
+
+            return integral_height + leftover_height;
+        } else {
+            cycle_finder.insert((part2.piecenum, part2.jetnum, shape), (height, drops));
+        }
+    }
 }
 
 fn parse() -> Vec<char> {
@@ -44,6 +95,8 @@ impl Chamber {
         [0b0000000, 0b0000000, 0b0011000, 0b0011000],
     ];
 
+    const OUCH: usize = 1_000_000_000_000;
+
     pub fn new(jets: Vec<char>) -> Self {
         Self {
             jets,
@@ -72,15 +125,15 @@ impl Chamber {
             match jet {
                 '<' => {
                     if self.can_go_left(bottom, &piece) {
-                        for i in 0..4 {
-                            piece[i] <<= 1;
+                        for p in piece.iter_mut() {
+                            *p <<= 1;
                         }
                     }
                 }
                 '>' => {
                     if self.can_go_right(bottom, &piece) {
-                        for i in 0..4 {
-                            piece[i] >>= 1;
+                        for p in piece.iter_mut() {
+                            *p >>= 1;
                         }
                     }
                 }
