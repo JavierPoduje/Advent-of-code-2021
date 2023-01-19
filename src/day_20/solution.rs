628cea7646552428422062b5efd783bd09ebf59d
JavierPoduje <javier.poduje@gmail.com>
2023-01-18 22:49:06 -0300

    feat: add 2022's day 20 - part 2

--- src/day_20/solution.rs
+++ src/day_20/solution.rs
@@ -2,36 +2,17 @@ use super::super::utils::read_one_per_line::read_one_per_line;
 
 pub fn solution() -> (String, String) {
     let data = parse();
-    let part1 = part1(data);
-    (part1.to_string(), "B".to_string())
+    let part1 = part1(data.clone());
+    let part2 = part2(data);
+    (part1.to_string(), part2.to_string())
 }
 
 fn part1(data: Vec<(i64, i64)>) -> i64 {
-    let mut result = data.clone();
+    decrypt(&data, 1, 1)
+}
 
-    let len = data.len() as i64 - 1;
-
-    for d in data {
-        let pos = result.iter().position(|n| n == &d).unwrap() as i64;
-        let mut new_pos = (pos + d.1) % len;
-
-        if new_pos < 0 {
-            new_pos += len;
-        }
-
-        if new_pos >= len {
-            new_pos -= len;
-        }
-
-        let val = result.remove(pos as usize);
-        result.insert(new_pos as usize, val);
-    }
-
-    let zero = result.iter().position(|p| p.1 == 0).unwrap();
-
-    result[(zero + 1000) % result.len()].1
-        + result[(zero + 2000) % result.len()].1
-        + result[(zero + 3000) % result.len()].1
+fn part2(data: Vec<(i64, i64)>) -> i64 {
+    decrypt(&data, 10, 811589153)
 }
 
 fn parse() -> Vec<(i64, i64)> {
@@ -43,4 +24,40 @@ fn parse() -> Vec<(i64, i64)> {
         .enumerate()
         .map(|n| (n.0 as i64, n.1))
         .collect()
+}
+
+fn decrypt(data: &Vec<(i64, i64)>, rounds: usize, key: i64) -> i64 {
+    let data = data
+        .iter()
+        .map(|(index, value)| (*index, value * key))
+        .collect::<Vec<_>>();
+
+    let mut result = data.clone();
+
+    let len = result.len() as i64 - 1;
+
+    for _ in 0..rounds {
+        for d in &data {
+            let pos = result.iter().position(|n| n == d).unwrap() as i64;
+
+            let mut new_pos = (pos + d.1) % len;
+
+            if new_pos < 0 {
+                new_pos += len;
+            }
+
+            if new_pos >= len {
+                new_pos -= len;
+            }
+
+            let val = result.remove(pos as usize);
+            result.insert(new_pos as usize, val);
+        }
+    }
+
+    let zero = result.iter().position(|p| p.1 == 0).unwrap();
+
+    result[(zero + 1000) % result.len()].1
+        + result[(zero + 2000) % result.len()].1
+        + result[(zero + 3000) % result.len()].1
 }
