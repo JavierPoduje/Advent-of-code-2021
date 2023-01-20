00debccebf6d70ec7adceafd43c9dbc5964a05e6
JavierPoduje <javier.poduje@gmail.com>
2023-01-19 21:25:46 -0300

    feat: add 2022's day 21 - part 2

--- src/day_21/solution.rs
+++ src/day_21/solution.rs
@@ -4,12 +4,63 @@ use super::super::utils::read_one_per_line::read_one_per_line;
 
 pub fn solution() -> (String, String) {
     let tree = parse();
-    let part1 = part1(tree);
-    (part1.to_string(), "B".to_string())
+    let part1 = part1(tree.clone());
+    let part2 = part2(tree);
+    (part1.to_string(), part2.to_string())
 }
 
 fn part1(tree: HashMap<String, Monkey>) -> i64 {
     eval(&"root".to_string(), &tree)
+}
+
+fn part2(tree: HashMap<String, Monkey>) -> i64 {
+    let root = "root".to_string();
+    let path = find_human(&root, &tree).unwrap();
+    let path = path.iter().rev().copied().collect::<Vec<_>>();
+
+    let (left, right) = match tree.get(&root).unwrap() {
+        Monkey::Value(_) => panic!("root monkey has no data"),
+        Monkey::Binary(left, _, right) => (left, right),
+    };
+
+    let correct_val = if left == path[1] {
+        eval(right, &tree)
+    } else {
+        eval(left, &tree)
+    };
+
+    find_adjustment(&path, 1, &tree, correct_val)
+}
+
+fn find_adjustment(
+    path: &Vec<&String>,
+    index: usize,
+    tree: &HashMap<String, Monkey>,
+    cv: i64,
+) -> i64 {
+    match tree.get(path[index]).unwrap() {
+        Monkey::Value(_) => cv,
+        Monkey::Binary(l, op, r) => {
+            let left = eval(l, tree);
+            let right = eval(r, tree);
+            let new_cv = if l == path[index + 1] {
+                match op {
+                    Operation::Addition => cv - right,
+                    Operation::Subtraction => cv + right,
+                    Operation::Multiplication => cv / right,
+                    Operation::Division => cv * right,
+                }
+            } else {
+                match op {
+                    Operation::Addition => cv - left,
+                    Operation::Subtraction => left - cv,
+                    Operation::Multiplication => cv / left,
+                    Operation::Division => left / cv,
+                }
+            };
+            find_adjustment(path, index + 1, tree, new_cv)
+        }
+    }
 }
 
 fn eval(start: &String, tree: &HashMap<String, Monkey>) -> i64 {
@@ -26,6 +77,25 @@ fn eval(start: &String, tree: &HashMap<String, Monkey>) -> i64 {
             }
         }
     }
+}
+
+fn find_human<'a>(loc: &'a String, tree: &'a HashMap<String, Monkey>) -> Option<Vec<&'a String>> {
+    if loc == "humn" {
+        return Some(vec![loc]);
+    }
+
+    if let Some(Monkey::Binary(l, _, r)) = tree.get(loc) {
+        if let Some(mut vec) = find_human(l, tree) {
+            vec.push(loc);
+            return Some(vec);
+        }
+        if let Some(mut vec) = find_human(r, tree) {
+            vec.push(loc);
+            return Some(vec);
+        }
+    }
+
+    None
 }
 
 fn parse() -> HashMap<String, Monkey> {
@@ -45,32 +115,30 @@ fn parse() -> HashMap<String, Monkey> {
             let expr = right.split_whitespace().collect::<Vec<_>>();
             tree.insert(
                 left.to_string(),
-                Monkey::Binary(expr[0].to_string(),
-                Operation::parse(
-                    expr[1].chars().next().unwrap()),
+                Monkey::Binary(
+                    expr[0].to_string(),
+                    Operation::parse(expr[1].chars().next().unwrap()),
                     expr[2].to_string(),
-                )
+                ),
             );
         }
     }
 
-    println!("{tree:?}");
-
     tree
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Monkey {
     Value(i64),
     Binary(String, Operation, String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Operation {
     Addition,
     Subtraction,
     Multiplication,
-    Division
+    Division,
 }
 
 impl Operation {
